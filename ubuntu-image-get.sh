72d9495b77f8317aa1eae270f051f3514300999a
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2023-02-22 16:49:16 +0100

    Update ubuntu-image-get.sh

--- ubuntu-image-get.sh
+++ ubuntu-image-get.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-sudo mkdir -p /datastore1/imgs
-sudo wget https://cloud-images.ubuntu.com/releases/22.10/release/ubuntu-22.10-server-cloudimg-amd64.img -O /datastore1/imgs/ubuntu-22.10-server.img
+sudo mkdir -p /opt/kvm/datastore1/imgs
+sudo wget https://cloud-images.ubuntu.com/releases/22.10/release/ubuntu-22.10-server-cloudimg-amd64.img -O /opt/kvm/datastore1/imgs/ubuntu-22.10-server.img
 
 echo "Resizing Ubuntu Image to 30GB"
 echo "-----------------------------"
 
-sudo qemu-img resize /datastore1/imgs/ubuntu-22.10-server.img 30G
-sudo qemu-img info /datastore1/imgs/ubuntu-22.10-server.img
+sudo qemu-img resize /opt/kvm/datastore1/imgs/ubuntu-22.10-server.img 30G
+sudo qemu-img info /opt/kvm/datastore1/imgs/ubuntu-22.10-server.img
+
+echo "Customizing Ubuntu Image"
+echo "------------------------"
+
+sudo virt-customize -a /opt/kvm/datastore1/imgs/ubuntu-22.10-server.img --install qemu-guest-agent
+sudo virt-customize -a /opt/kvm/datastore1/imgs/ubuntu-22.10-server.img --run-command "sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
