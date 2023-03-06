a573b4b567aaa4da59cd9dec817b9a2196adbea9
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2023-03-06 16:46:10 +0100

    Update 2-generate-cloud-init-isoimg.sh

--- 2-generate-cloud-init-isoimg.sh
+++ 2-generate-cloud-init-isoimg.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sudo genisoimage -output /opt/kvm/datastore1/imgs/ubuntu.iso -volid cidata -joliet -rock user-data meta-data network-config
+
+THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
+BASEDIR=`cat $THIS_DIR/variables.var | grep BASEDIR | awk -F= {'print $2'} | awk -F\' {'print $2'}`
+VM_NAME=`cat $THIS_DIR/variables.var | grep VM_NAME | awk -F= {'print $2'} | awk -F\' {'print $2'}`
+PASSWORD=`cat $THIS_DIR/variables.var | grep PASSWORD | awk -F= {'print $2'} | awk -F\' {'print $2'}`
+
+sed -i "s/password.*/password: $PASSWORD/" $THIS_DIR/user-data
+sudo genisoimage -output $BASEDIR/$VM_NAME.iso -volid cidata -joliet -rock user-data meta-data network-config
