c5267963783d002cfa5fd9c5f7d173d18a625650
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2023-03-06 16:45:39 +0100

    Update 1-ubuntu-image-get.sh

--- 1-ubuntu-image-get.sh
+++ 1-ubuntu-image-get.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-sudo mkdir -p /opt/kvm/datastore1/imgs
-sudo wget https://cloud-images.ubuntu.com/releases/22.10/release/ubuntu-22.10-server-cloudimg-amd64.img -O /opt/kvm/datastore1/imgs/ubuntu-22.10-server.img
+THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
+BASEDIR=`cat $THIS_DIR/variables.var | grep BASEDIR | awk -F= {'print $2'} | awk -F\' {'print $2'}`
+VM_NAME=`cat $THIS_DIR/variables.var | grep VM_NAME | awk -F= {'print $2'} | awk -F\' {'print $2'}`
+PASSWORD=`cat $THIS_DIR/variables.var | grep PASSWORD | awk -F= {'print $2'} | awk -F\' {'print $2'}`
+
+sudo mkdir -p $BASEDIR
+sudo wget https://cloud-images.ubuntu.com/releases/22.10/release/ubuntu-22.10-server-cloudimg-amd64.img -O $BASEDIR/$VM_NAME.img
 
 echo "Resizing Ubuntu Image to 30GB"
 echo "-----------------------------"
 
-sudo qemu-img resize /opt/kvm/datastore1/imgs/ubuntu-22.10-server.img 30G
-sudo qemu-img info /opt/kvm/datastore1/imgs/ubuntu-22.10-server.img
+sudo qemu-img resize $BASEDIR/$VM_NAME.img 30G
+sudo qemu-img info $BASEDIR/$VM_NAME.img
 
 #echo "Customizing Ubuntu Image"
 #echo "------------------------"
