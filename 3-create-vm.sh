eea26b3285f47483945fc4608f636b060e337190
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2023-03-23 09:56:13 +0100

    Update 3-create-vm.sh

--- 3-create-vm.sh
+++ 3-create-vm.sh
@@ -7,4 +7,4 @@ PASSWORD=`cat $THIS_DIR/variables.var | grep PASSWORD | awk -F= {'print $2'} | a
 VRAM=`cat $THIS_DIR/variables.var | grep VRAM | awk -F= {'print $2'} | awk -F\' {'print $2'}`
 VCPUS=`cat $THIS_DIR/variables.var | grep VCPUS | awk -F= {'print $2'} | awk -F\' {'print $2'}`
 
-sudo virt-install --import --name ubuntuk8s --memory $VRAM --vcpus $VCPUS --disk $BASEDIR/$VM_NAME.img,format=qcow2,bus=virtio --cdrom=$BASEDIR/$VM_NAME.iso --network bridge=br0 --osinfo=ubuntu22.10 --graphics vnc  &
+sudo virt-install --import --name $VM_NAME --memory $VRAM --vcpus $VCPUS --disk $BASEDIR/$VM_NAME.img,format=qcow2,bus=virtio --cdrom=$BASEDIR/$VM_NAME.iso --network bridge=br0 --osinfo=ubuntu22.10 --graphics vnc  &
